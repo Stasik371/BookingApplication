4b89669a4c041f0965832c92c0e937fcb8883888
Chapalax <max250903@gmail.com>
2022-09-30 01:05:48 +0300

    First commit.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.main.bookingapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.main.bookingapplication to javafx.fxml;
+    exports com.main.bookingapplication;
+    exports controllers;
+    opens controllers to javafx.fxml;
+}
